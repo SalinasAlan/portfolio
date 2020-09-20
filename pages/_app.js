63e21edbccc723bebf87e276a8922201839c2bcd
Alan <alansal_11@hotmail.com>
2020-09-19 19:16:09 -0500

    Theme provider was added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+    // primary: "#293241",
+    primary: "#1F1F1F",
+    secundary: "#2F3E46",
+    textColor: "#F2F4F4",
+    // textColor: "#E0FBFC",
+    textColorSecondary: "#EE6C4D"
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default MyApp
