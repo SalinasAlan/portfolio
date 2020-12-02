ae645a75f3ad966e7e5943acb190fe088e542db2
Alan <alansal_11@hotmail.com>
2020-12-02 12:16:22 -0600

    pageview in useEffect hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import '../styles/globals.css'
 import { ThemeProvider } from 'styled-components'
 
@@ -11,6 +14,19 @@ const theme = {
 }
 
 function MyApp({ Component, pageProps }) {
+
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return (
         <>
             <ThemeProvider theme={theme}>
